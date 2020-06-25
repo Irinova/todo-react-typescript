6c6bf205923276529139edbf01960ca0a828bc27
Irina Zakharova <letter.zakharova@ya.ru>
2020-06-25 23:38:31 +0300

    'start'

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Todo list (React + typeScript)
+
+Для удобства каждый шаг сделан отдельной веткой:
+
+##### Шаг 1. 
+
+```
+git checkout step-1
+```
+Добавление компонентов NewTask и TasksList.
+
+
+
+
+
---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+const App = () => {
+
+    return (
+        <>
+            <h1>
+                Hello, React + Typescript!
+            </h1>
+        </>
+    )
+}
+
+export default App;
